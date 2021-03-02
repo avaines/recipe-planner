85a3bb638c9a6cf223c164d1366de7bdeb046fc1
Aiden Vaines <aiden@vaines.org>
2021-03-02 12:21:49 +0000

    Tutorial (#3)
    * initial sign in stuff
    
    * Adding env vars
    
    * Fixing pipeline

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./routes/index";
+import firebase from "firebase";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+firebase.initializeApp({
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
+});
+
+firebase.auth().onAuthStateChanged(user => {
+  store.dispatch("fetchUser", user);
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
