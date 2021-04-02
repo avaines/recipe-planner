a4292a0618a4349f5f41ccd8dc52558c55f19338
n3rden <aiden@vaines.org>
2021-04-02 16:02:01 +0100

    adding counted ingredients

--- src/helpers/ObjectFunctions.js
+++ src/helpers/ObjectFunctions.js
@@ -1,13 +1,26 @@
 export default {
     getUniqueIngredients(l) {
-        const ingredients = [];
+        const ingredients_raw = [];
+        // Create a list object of ingredients from the csv list
         l.forEach(function (item) {
             item.ingredients.forEach(function (listItem) {
-                if (listItem != '' && !ingredients.includes(listItem) ) {
-                    ingredients.push(listItem);
-                }
+                // if (listItem != '' && !ingredients.includes(listItem) ) {
+                    ingredients_raw.push(listItem);
+                // }
             });
         });
+
+        // Get the list of how many times a unique ingredient appears in the list
+        const ingredient_counts = {};
+        ingredients_raw.forEach(function(x) {
+            ingredient_counts[x] = (ingredient_counts[x] || 0)+1;
+        });
+
+        const ingredients = [];
+        for (const [key, value] of Object.entries(ingredient_counts)) {
+            ingredients.push(`${key} (${value})`)
+            console.log(`${key} (${value})`);
+          }
 
         return ingredients.sort();
     }
